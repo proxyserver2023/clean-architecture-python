3b0d349d14d48712051e74f6c68a3ac9d8898b87
Md. Alamin Mahamud <alamin.ineedahelp@gmail.com>
2019-04-26 22:06:20 +0600

    average func feature with tests

--- calc/calc/calc.py
+++ calc/calc/calc.py
@@ -21,3 +21,16 @@ class Calc:
 	
 	def div(self, a, b):
 		return a/b
+
+	def avg(self, *args, ut=None, lt=None):
+
+		if not args:
+			return 0
+
+		if not ut:
+			ut = max(args)
+		if not lt:
+			lt = min(args)
+
+		args = [arg for arg in args if arg >= lt and arg <= ut]
+		return (sum(args)/len(args)) if args else 0
--- calc/tests/test_calc.py
+++ calc/tests/test_calc.py
@@ -89,8 +89,41 @@ def test_multiplication_arg_if_zero_should_raise_value_error():
 """
 
 def test_division():
-	assert abs(calc.Calc().div(10, 3) - 3.333333) <= 0.001
+	assert calc.Calc().div(10, 3) == pytest.approx(3.333333)
 
 def test_division_with_zero_raises_exception():
 	with pytest.raises(ZeroDivisionError):
 		calc.Calc().div(10, 0)
+
+
+"""Average Tests
+"""
+
+def test_avg_takes_iterable_and_computes_the_average():
+	assert calc.Calc().avg(*(range(1,11))) == pytest.approx(5.5)
+
+
+def test_avg_arguments_should_not_be_greater_than_optional_upper_threshold():
+	assert calc.Calc().avg(*(range(1,11)), 1000, 2000, ut=100) == pytest.approx(5.5)
+
+
+def test_avg_arguments_should_not_be_lower_than_optional_lower_threshold():
+	assert calc.Calc().avg(*(range(1,11)), lt=3) == pytest.approx(6.5)
+
+def test_avg_upper_threshold_are_included():
+	assert calc.Calc().avg(*(range(1,11)), 98, ut=98) == pytest.approx(13.909090)
+
+def test_avg_lower_threshold_are_included():
+	assert calc.Calc().avg(*(range(1,11)), lt=1) == pytest.approx(5.5)
+
+def test_avg_should_return_zero_on_empty_sequence():
+	assert calc.Calc().avg() == 0
+
+def test_avg_works_with_no_arguments():
+	assert calc.Calc().avg() == 0
+
+def test_avg_works_if_empty_list_after_outlier_removal():
+	assert calc.Calc().avg(12, 98, lt=15, ut=95) == 0
+
+def test_avg_outlier_removal_works_even_if_the_list_empty():
+	assert calc.Calc().avg(12, 98, lt=15, ut=95) == 0
