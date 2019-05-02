c907a00045440285e02cecd1d951b42008da6866
Md. Alamin Mahamud <alamin.ineedahelp@gmail.com>
2019-05-02 08:43:41 +0600

    patching done

--- fileinfo/tests/test_fileinfo.py
+++ fileinfo/tests/test_fileinfo.py
@@ -10,6 +10,7 @@ Tests for `fileinfo` module.
 
 import pytest
 from fileinfo.fileinfo import FileInfo
+from unittest.mock import patch
 
 
 def test_init():
@@ -23,3 +24,13 @@ def test_init_relative():
     relative_path = f'../{filename}'
     f = FileInfo(relative_path)
     assert f.filename == filename
+
+
+def test_get_info():
+    filename = 'somefile.ext'
+    original_path = '../{}'.format(filename)
+    with patch('os.path.abspath') as abspath_mock:
+        test_abspath = 'some/abs/path'
+        abspath_mock.return_value = test_abspath
+        fi = FileInfo(original_path)
+        assert fi.get_info() == (filename, original_path, test_abspath)
