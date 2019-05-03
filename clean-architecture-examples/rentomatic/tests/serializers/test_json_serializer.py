d67c1f8c59e261df0d85b772c9d636e190eaad60
Md. Alamin Mahamud <alamin.ineedahelp@gmail.com>
2019-05-03 18:13:19 +0600

    serializers for our domain

---
+++ clean-architecture-examples/rentomatic/tests/serializers/test_json_serializer.py
@@ -0,0 +1,30 @@
+import json
+import uuid
+from rentomatic.serializers import room_json_serializer as ser
+from rentomatic.domain import room as r
+
+
+def test_serialize_domain_room():
+    code = uuid.uuid4()
+    room = r.Room(
+        code=code,
+        size=200,
+        price=10,
+        longitude=-0.09998975,
+        latitude=51.75436293
+    )
+
+    expected_json = """
+    {{
+        "code": "{}",
+        "size": 200,
+        "price": 10,
+        "longitude": -0.09998975,
+        "latitude": 51.75436293
+    }}
+    """.format(code)
+
+    jsonified_room = json.dumps(room, cls=ser.RoomJSONEncoder)
+
+    # deserializing into python dict for ignoring attribute order
+    assert json.loads(jsonified_room) == json.loads(expected_json)
